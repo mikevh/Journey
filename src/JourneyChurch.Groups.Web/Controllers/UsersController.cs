a9f792bbd6a4e82012625b44f629f1c1cd0e2370
Michael V <michaelv@attunix.com>
2015-09-09 14:58:46 -0700

    membership

---
+++ src/JourneyChurch.Groups.Web/Controllers/UsersController.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using JourneyChurch.Groups.Web.Models;
+using JourneyChurch.Groups.Web.ViewModels.User;
+using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Mvc;
+
+namespace JourneyChurch.Groups.Web.Controllers
+{
+    [Route("api/[controller]")]
+    public class UsersController : Controller
+    {
+        private readonly UserManager<JourneyUser> _userManager;
+
+        public UsersController(UserManager<JourneyUser> userManager) {
+            _userManager = userManager;
+
+            if (!_userManager.Users.Any()) {
+
+                var admin = _userManager.CreateAsync(new JourneyUser {UserName = "admin", Email = "mikevh@gmail.com"}).Result;
+            }
+        }
+
+        [HttpGet]
+        public IActionResult GetAll() {
+            var rv = _userManager.Users.Select(u => new UserViewModel
+            {
+                Id = u.Id,
+                UserName = u.UserName,
+                Email = u.Email
+            });
+
+            return new ObjectResult(rv);
+        }
+    }
+}
