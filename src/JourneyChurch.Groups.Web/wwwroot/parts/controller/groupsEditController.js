8349ae295ccd527979fcce1ffafc1538347a81d1
Michael V <michaelv@attunix.com>
2015-09-16 07:00:52 -0700

    basic group edit

--- src/JourneyChurch.Groups.Web/wwwroot/parts/controller/groupsEditController.js
+++ src/JourneyChurch.Groups.Web/wwwroot/parts/controller/groupsEditController.js
@@ -2,44 +2,20 @@
 
     $scope.groupId = $routeParams.id;
 
-    //$scope.daysOfWeek = _.map($locale.DATETIME_FORMATS.DAY, function (x, i) {
-    //    return { Id: i, Name: x };
-    //});
+    $scope.daysOfWeek = _.map($locale.DATETIME_FORMATS.DAY, function (x, i) {
+        return { Id: i, Name: x };
+    });
 
-    //$scope.dayOfWeekName = function(i) {
-    //    return $locale.DATETIME_FORMATS.DAY[i];
-    //};
+    $scope.save = function () {
+         $scope.e.$update().then(function() {
+            $location.path('/');
+        });
+    };
 
-    //$scope.saveNew = function () {
-    //    var entry = new Group();
-    //    angular.extend(entry, $scope.n);
-    //    entry.$save().then(function (data) {
-    //        $scope.groups.push(data);
-    //        $scope.cancelNew();
-    //    });
-    //};
+    $scope.cancel = function () {
+        $location.path('/');
+    };
 
-    //$scope.cancelNew = function () {
-    //    $scope.showNew = false;
-    //};
-
-    //$scope.add = function () {
-    //    $scope.n = {
-    //        name: '',
-    //        leader: '',
-    //        meetsOn: 4
-    //    };
-    //    $scope.showNew = true;
-    //};
-
-    //$scope.remove = function (g) {
-    //    g.$delete().then(function () {
-    //        var index = $scope.groups.indexOf(g);
-    //        $scope.groups.splice(index, 1);
-    //    });
-    //}
-    console.log($scope.groupId);
-    
     Group.get({ id: $scope.groupId }).$promise.then(function (data) {
         $scope.e = data;
     });
