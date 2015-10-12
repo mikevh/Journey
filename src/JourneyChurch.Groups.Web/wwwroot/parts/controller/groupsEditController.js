7ee91477e5626bb9b43ad473e19b81180c1bcec2
Michael V <michaelv@attunix.com>
2015-10-11 19:38:31 -0700

    Groups

--- src/JourneyChurch.Groups.Web/wwwroot/parts/controller/groupsEditController.js
+++ src/JourneyChurch.Groups.Web/wwwroot/parts/controller/groupsEditController.js
@@ -1,5 +1,5 @@
 ﻿angular.module('app').controller('groupsEditController', function ($scope, $locale, $routeParams, $location, Group) {
-
+    
     $scope.groupId = $routeParams.id;
 
     $scope.daysOfWeek = _.map($locale.DATETIME_FORMATS.DAY, function (x, i) {
@@ -16,8 +16,20 @@
         $location.path('/');
     };
 
-    Group.get({ id: $scope.groupId }).$promise.then(function (data) {
-        $scope.e = data;
-    });
-
+    if($scope.groupId > 0) {
+        Group.get({ id: $scope.groupId }).$promise.then(function (data) {
+            $scope.e = data;
+        });
+    }        
+    else {
+        $scope.e = new Group();
+        angular.extend($scope.e, 
+        {
+            id: 0,
+            name: 's',
+            leader: 'sd',
+            meetsOn: 4,
+            notes: 'ff'
+        });
+    }
 });
