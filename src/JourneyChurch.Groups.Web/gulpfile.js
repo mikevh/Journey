98023031bb5bd24d3dcd398d769b3b1cc92da807
Michael V <michaelv@attunix.com>
2015-10-13 16:15:50 -0700

    gulp

--- src/JourneyChurch.Groups.Web/gulpfile.js
+++ src/JourneyChurch.Groups.Web/gulpfile.js
@@ -5,20 +5,23 @@ var gulp = require("gulp"),
     concat = require("gulp-concat"),
     cssmin = require("gulp-cssmin"),
     uglify = require("gulp-uglify"),
+    angularFilesort = require("gulp-angular-filesort"),
+    inject = require("gulp-inject"),
+    watch = require("gulp-watch"),
     project = require("./project.json");
 
 var paths = {
     webroot: "./" + project.webroot + "/"
 };
 
-paths.js = paths.webroot + "js/**/*.js";
-paths.minJs = paths.webroot + "js/**/*.min.js";
+paths.js = paths.webroot + "parts/**/*.js";
+paths.minJs = paths.webroot + "parts/**/*.min.js";
 paths.css = paths.webroot + "css/**/*.css";
 paths.minCss = paths.webroot + "css/**/*.min.css";
-paths.concatJsDest = paths.webroot + "js/site.min.js";
+paths.concatJsDest = paths.webroot + "parts/site.min.js";
 paths.concatCssDest = paths.webroot + "css/site.min.css";
 
-gulp.task("clean:js", function (cb) {
+gulp.task("clean:js", function (cb) { 
     rimraf(paths.concatJsDest, cb);
 });
 
@@ -30,6 +33,7 @@ gulp.task("clean", ["clean:js", "clean:css"]);
 
 gulp.task("min:js", function () {
     gulp.src([paths.js, "!" + paths.minJs], { base: "." })
+        .pipe(inject(gulp.src([paths.js]).pipe(angularFilesort())))
         .pipe(concat(paths.concatJsDest))
         //.pipe(uglify())
         .pipe(gulp.dest("."));
@@ -38,8 +42,12 @@ gulp.task("min:js", function () {
 gulp.task("min:css", function () {
     gulp.src([paths.css, "!" + paths.minCss])
         .pipe(concat(paths.concatCssDest))
-        //.pipe(cssmin())
+        .pipe(cssmin())
         .pipe(gulp.dest("."));
 });
 
 gulp.task("min", ["min:js", "min:css"]);
+
+gulp.task('watch', function() {
+    return gulp.watch([paths.js, paths.css], ['min']);
+});
